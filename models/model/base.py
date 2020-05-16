fix masking inversion for pytorch 1.1

models/model/base.py
@@ -397,7 +397,7 @@ class AlfredDataset(Dataset):
                 feat[k] = pad_seq
             elif k in {'lang_goal_instr_len', 'lang_instr_len'}:
                 feat[k] = torch.tensor(v, dtype=torch.long)
-            elif k in {'lang_instr_subgoal_mask'}:
+            elif k in {'lang_instr_subgoal_mask', 'lang_goal_instr_subgoal_mask'}:
                 seqs = [torch.tensor(vv, dtype=torch.bool) for vv in v]
                 pad_seq = pad_sequence(seqs, batch_first=True, padding_value=False)
                 feat[k] = pad_seq

models/nn/vnn.py
@@ -37,7 +37,8 @@ class DotAttn(nn.Module):
         if mask is not None:
             mask = mask.unsqueeze(-1)
             assert mask.size() == raw_score.size(), (mask.size(), raw_score.size())
-            raw_score.masked_fill_(~mask, BIG_NEG)
+            # ~ not implemented on bool tensors in 1.1
+            raw_score.masked_fill_(~(mask.byte()), BIG_NEG)
         self.raw_score = raw_score
         score = F.softmax(raw_score, dim=1)
         return score