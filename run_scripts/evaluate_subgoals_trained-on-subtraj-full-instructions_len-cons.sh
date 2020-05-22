bac57c6e0ca9934b7df9c4d0732b2e745b1a579e
Coline Devin <coline.devin@gmail.com>
2020-05-22 23:55:09 +0000

    Run scripts

---
+++ run_scripts/evaluate_subgoals_trained-on-subtraj-full-instructions_len-cons.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export DISPLAY=":0"
+export ALFRED_ROOT="`pwd`"
+
+source activate alfred
+
+model_dir=$1
+
+subgoals=$2
+if [[ -z $subgoals ]]
+then
+  subgoals=all
+fi
+
+for split in valid_seen valid_unseen
+#for split in valid_seen
+do
+  python -u models/eval/eval_seq2seq.py \
+    --model_path ${model_dir}/best_seen.pth \
+    --eval_split $split \
+    --data data/json_feat_2.1.0 \
+    --model models.model.seq2seq_im_mask \
+    --gpu \
+    --num_threads 3 \
+    --subgoals $subgoals \
+    --trained_on_subtrajectories \
+    --trained_on_subtrajectories_full_instructions \
+    --subgoals_length_constrained \
+    --skip_model_unroll_with_expert \
+    --eval_type subgoals \
+    | tee ${model_dir}/eval_subtraj-trained-full-instructions_subgoals_${subgoals}_len-cons_${split}.out
+done
